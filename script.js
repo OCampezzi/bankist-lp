19c3e34e2c551b2e27e570d1248e70cfa013209d
Otávio Campezzi <90792115+OCampezzi@users.noreply.github.com>
2025-03-10 11:44:55 -0300

    Created a smooth scrolling function to the "Learn more" btn

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnLearnMore = document.querySelector('.btn--scroll-to');
+const featuresSection = document.querySelector('#section--1');
+const operationsSection = document.querySelector('#section--2');
+const testimonialsSection = document.querySelector('#section--3');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +23,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +32,8 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+btnLearnMore.addEventListener('click', function () {
+  featuresSection.scrollIntoView({ behavior: 'smooth' });
 });
